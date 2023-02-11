a9035a147f89015776d5822741d7fb039f767425
xaviqo <silent90s@gmail.com>
2023-02-11 11:07:28 +0100

    First commit

---
+++ src/main/java/tech/xavi/exchangeapi/ExchangeApiApplication.java
@@ -0,0 +1,13 @@
+package tech.xavi.exchangeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/xavi/exchangeapi/ExchangeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.xavi.exchangeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
